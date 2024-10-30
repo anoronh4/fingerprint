baf75536912100026122c40910c43fcaaf925840
Noronha <noronhaa@crater2.dmp.private>
2024-10-30 17:40:03 -0400

    bugfix

--- gbcms.sh
+++ gbcms.sh
@@ -1,13 +1,14 @@
 sample=$1
 bam=$(readlink -f $2)
 outdir=$3
-fasta=$4
-variant_input=$5 
+fasta=$(readlink -f $4)
+variant_input=$(readlink -f $5) 
 #    fasta=/juno/work/tempo/cmopipeline/mskcc-igenomes/igenomes/Homo_sapiens/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa
 #    fasta=/juno/work/ci/resources/genomes/GRCh37/fasta/b37.fasta
 #variant_input=/juno/work/ccs/noronhaa/tools/fingerprint/FP.vcf
 output=$sample.vcf
 
+mkdir -p $outdir
 singularity exec -e --no-home -B $outdir -B $(dirname $fasta) -B $(dirname $bam) -B $(dirname $variant_input) docker://ghcr.io/msk-access/gbcms:1.2.5 \
 GetBaseCountsMultiSample \
      --maq 0 \
